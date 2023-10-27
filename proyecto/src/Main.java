51ba6fc3eb8b7249a15b817be9ddd0dbcbfceb90
Ana <avalladaresgonzalez@danielcastelao.org>
2023-10-27 09:22:47 +0200

    Añadiendo Main

---
+++ proyecto/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        float lado = 3;
+        //float area = lado*lado;
+
+        // o también
+
+        float area = (float) Math.pow(lado, 2);
+        System.out.println("O area dun cadrado de lado 3 é: " + area);
+    }
+}
